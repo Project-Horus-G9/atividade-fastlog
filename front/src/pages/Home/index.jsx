2ea6deabbd1cfb19793453daa84cce950956eded
VictorRubinec <vrubinec@gmail.com>
2024-11-11 20:10:23 -0300

    adicionanod package.json

--- front/src/pages/Home/index.jsx
+++ front/src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { style } from "./style.js";
+import { Box, Button, Typography } from "@material-ui/core";
 
 function Home() {
-
+// interface simples onde os clientes possam inserir o número de rastreamento e visualizar o status da entrega.
     return (
-        <></>
+        <Box sx={style.box}>
+            <Typography variant="h4" sx={style.typography}>Rastreamento de encomendas</Typography>
+            <Button variant="contained" color="primary" sx={style.button}>Rastrear</Button>
+        </Box>
     );
 }
 
--- front/src/pages/Home/style.js
+++ front/src/pages/Home/style.js
@@ -1,6 +1,18 @@
 const Style = () => {
     return {
-        
+        box: {
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+        },
+        typography: {
+            marginBottom: 20,
+        },
+        button: {
+            width: 200,
+        },
     };
 };
 
