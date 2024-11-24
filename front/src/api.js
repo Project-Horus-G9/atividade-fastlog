1ec3c10bb5f83a353f7b9add8b36c19a1381c074
VictorRubinec <vrubinec@gmail.com>
2024-11-24 19:39:00 -0300

    mudando api.js

--- front/src/api.js
+++ front/src/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const ENDPOINT = "localhost:8080" || process.env.ENDPOINT;
+const ENDPOINT = process.env.REACT_APP_ENDPOINT || "localhost:8080";
 
 const api = axios.create({
   baseURL: `http://${ENDPOINT}`,
 });
 
+console.log(`http://${ENDPOINT}`);
 
 export default api;
